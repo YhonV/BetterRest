b163743b0f6ad2dcd2206223dcb0193f3cbb2fb0
Yhon Vivas <cactu@Mac-mini-de-Yhon.local>
2025-12-31 15:52:36 -0300

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yhon Vivas on 29-12-25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
